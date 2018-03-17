26fea020b1de4f4f01f5f58dca07c8a009c232aa
andre alves garzia <andre@andregarzia.com>
2018-03-16 21:01:50 -0300

    module(lexicon): work in progress.

---
+++ web-modules/lexicon/src/Page/Definition.elm
@@ -0,0 +1,6 @@
+module Definition exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (class, defaultValue, href, property, src, target)
+import Lexicon exposing (WordDefinition)
+
